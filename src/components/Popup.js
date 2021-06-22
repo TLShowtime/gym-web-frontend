9831ade015ffe0d16069ccebcde5fee10c0c3386
Zhong <liuguo35@hotmail.com>
2021-06-21 23:49:18 -0600

    Implemented popup

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
+import './Popup.css';
+import {Button, ButtonCancel} from './Button';
 
 function Popup(props) {
+    
+
     return (props.trigger) ? (
         <div className='popup'>
             <div className='popup-inner'>
-                <button className='close-btn'>Close</button>
+                
                 {props.children}
+                <Button buttonStyle='btn--confirm' onClick={()=> props.setTrigger(false)}>{props.mensajeConfirmacion}</Button>
+                <ButtonCancel buttonStyle='btn--confirm'>{props.mensajeCancelar}</ButtonCancel>
             </div>
         </div>
     ) : "";
